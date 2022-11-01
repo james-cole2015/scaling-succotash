83417b8fbab7e1bff083270544226932cafb2d6c
brooks <brooks.houck@gmail.com>
2022-11-01 14:49:09 +0000

    terraform fmt

--- modules/vpc2/variables.tf
+++ modules/vpc2/variables.tf
@@ -1,28 +1,28 @@
 variable "vpc_2_name" {
-    type = string
-    default = "darling-wasp"
+  type    = string
+  default = "darling-wasp"
 }
 variable "vpc_2_cidr" {
-    type = string
-    default = "10.200.0.0/16"
+  type    = string
+  default = "10.200.0.0/16"
 }
 variable "vpc_2_azs" {
-    type = list(string)
-    default = ["us-east-1a", "us-east-1b"]
+  type    = list(string)
+  default = ["us-east-1a", "us-east-1b"]
 }
 variable "vpc_2_private_subnets" {
-    type = list(string)
-    default = ["10.200.0.0/19", "10.200.32.0/19"]
+  type    = list(string)
+  default = ["10.200.0.0/19", "10.200.32.0/19"]
 }
 variable "vpc_2_public_subnets" {
-    type = list(string)
-    default = ["10.200.64.0/19", "10.200.96.0/19"]
+  type    = list(string)
+  default = ["10.200.64.0/19", "10.200.96.0/19"]
 }
 variable "vpc_2_database_subnets" {
-    type = list(string)
-    default = ["10.200.128.0/19", "10.200.160.0/19"]
+  type    = list(string)
+  default = ["10.200.128.0/19", "10.200.160.0/19"]
 }
 variable "vpc_2_intra_subnets" {
-    type = list(string)
-    default = ["10.200.192.0/19", "10.200.224.0/19"]
+  type    = list(string)
+  default = ["10.200.192.0/19", "10.200.224.0/19"]
 }
--- modules/vpc_1/variables.tf
+++ modules/vpc_1/variables.tf
@@ -1,28 +1,28 @@
 variable "vpc_1_name" {
-    type = string
-    default = "incognito-monkey"
+  type    = string
+  default = "incognito-monkey"
 }
 variable "vpc_1_cidr" {
-    type = string
-    default = "10.100.0.0/16"
+  type    = string
+  default = "10.100.0.0/16"
 }
 variable "vpc_1_azs" {
-    type = list(string)
-    default = ["us-east-1a", "us-east-1b"]
+  type    = list(string)
+  default = ["us-east-1a", "us-east-1b"]
 }
 variable "vpc_1_private_subnets" {
-    type = list(string)
-    default = ["10.100.0.0/19", "10.100.32.0/19"]
+  type    = list(string)
+  default = ["10.100.0.0/19", "10.100.32.0/19"]
 }
 variable "vpc_1_public_subnets" {
-    type = list(string)
-    default = ["10.100.64.0/19", "10.100.96.0/19"]
+  type    = list(string)
+  default = ["10.100.64.0/19", "10.100.96.0/19"]
 }
 variable "vpc_1_database_subnets" {
-    type = list(string)
-    default = ["10.100.128.0/19", "10.100.160.0/19"]
+  type    = list(string)
+  default = ["10.100.128.0/19", "10.100.160.0/19"]
 }
 variable "vpc_1_intra_subnets" {
-    type = list(string)
-    default = ["10.100.192.0/19", "10.100.224.0/19"]
+  type    = list(string)
+  default = ["10.100.192.0/19", "10.100.224.0/19"]
 }
