d08501cc4aea058489f552e4eee0729a90906329
brooks <brooks.houck@gmail.com>
2022-11-01 14:15:49 +0000

    removing vpc2 for clarity

--- modules/vpc_2/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "vpc_2_name" {}
-variable "vpc_2_cidr" {}
-variable "vpc_2_azs" {}
-variable "vpc_2_private_subnets" {}
-variable "vpc_2_public_subnets" {}
-variable "vpc_2_database_subnets" {}
-variable "vpc_2_intra_subnets" {}
