21d4180ff6cb26da25e3123a3afe1e850cc0e962
brooks <brooks.houck@gmail.com>
2022-11-01 14:16:14 +0000

    removing vpc2

--- main.tf
+++ main.tf
@@ -1,14 +1,9 @@
-/*
+
 module "vpc1" {
-    source = "./modules/vpc_1"
-    azs = aws_data.aws_az_names.names["0:2"]
+  source = "./modules/vpc_1"
 }
 
-module "vpc2" {
-    source = "./modules/vpc_2"
-    azs = aws_data.aws_az_names.names["0:2"]
-}
-*/
+
 module "aws_data" {
   source = "./modules/data"
 }
