46c49c123ee18a1faa35ba633cf257a9a4f6039b
brooks <brooks.houck@gmail.com>
2022-11-10 21:28:35 +0000

    adding name to vpc

--- main.tf
+++ main.tf
@@ -1,7 +1,13 @@
 resource "aws_vpc" "requesting_vpc" {
   cidr_block = "10.100.0.0/16"
+  tags = {
+    Name = "requesting-vpc"
+  }
 }
 
 resource "aws_vpc" "accepting_vpc" {
   cidr_block = "10.200.0.0/16"
+    tags = {
+    Name = "accepting-vpc"
+  }
 }
