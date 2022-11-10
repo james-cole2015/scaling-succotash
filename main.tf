5d7630612d69bb622d846e44789be186b4ff9bd4
brooks <brooks.houck@gmail.com>
2022-11-10 21:30:33 +0000

    adding subnets to configuration

--- main.tf
+++ main.tf
@@ -11,3 +11,19 @@ resource "aws_vpc" "accepting_vpc" {
     Name = "accepting-vpc"
   }
 }
+
+resource "aws_subnet" "requesting_subnet" {
+  vpc_id = aws_vpc.requesting_vpc.id
+  cidr_block = "10.100.1.0/24"
+  tags = {
+    "Name" = "requesting-subnet"
+  }
+}
+
+resource "aws_subnet" "accepting_subnet" {
+  vpc_id = aws_vpc.accepting_vpc.id
+  cidr_block = "10.100.1.0/24"
+  tags = {
+    "Name" = "accepting-subnet"
+  }
+}
