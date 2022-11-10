8a09ab3e9ac85edc037b0106fed5ad4d0489d754
brooks <brooks.houck@gmail.com>
2022-11-10 21:39:16 +0000

    adding ec2 instances

--- main.tf
+++ main.tf
@@ -29,3 +29,39 @@ resource "aws_subnet" "accepting_subnet" {
     "Name" = "accepting-subnet"
   }
 }
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "requesting_ec2" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  subnet_id = aws_subnet.requesting_subnet.id
+
+  tags = {
+    Name = "RequestingEC2"
+  }
+}
+
+resource "aws_instance" "accepting_ec2" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  subnet_id = aws_subnet.accepting_subnet.id
+
+  tags = {
+    Name = "AcceptingEC2"
+  }
+}
