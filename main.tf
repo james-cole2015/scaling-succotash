76da0cc1c411082fd7cdbf4a0e78ccb42f95d9d6
brooks <brooks.houck@gmail.com>
2022-11-13 18:21:06 +0000

    pointing subnets to igw

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ resource "aws_default_route_table" "requesting-route-table" {
   default_route_table_id = aws_vpc.requesting_vpc.main_route_table_id
 
   route {
-    cidr_block = "10.100.0.0/16"
+    cidr_block = "10.100.0.0/24"
     gateway_id = aws_internet_gateway.requesting-igw.id
   }
 }
@@ -66,7 +66,7 @@ resource "aws_default_route_table" "accepting-route-table" {
   default_route_table_id = aws_vpc.accepting_vpc.main_route_table_id
 
   route {
-    cidr_block = "10.200.0.0/16"
+    cidr_block = "10.200.0.0/24"
     gateway_id = aws_internet_gateway.accepting-igw.id
   }
 }
